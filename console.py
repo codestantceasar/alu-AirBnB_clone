adc168ff0f7497112a8bc2f7f27324f32520426e
codestantcaesar <c.akas@alustudent.com>
2025-05-14 16:02:08 +0200

    add new script

--- console.py
+++ console.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
@@ -16,6 +18,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when the line is empty"""
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()

f291f568e75c40b44fccc78a2889617f8d5afd6c
codestantcaesar <c.akas@alustudent.com>
2025-05-14 17:41:02 +0200

    Add this files

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,23 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self):
+        """Initializes a new instance of BaseModel."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns the string representation of the BaseModel instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates the `updated_at` attribute with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary representation of the instance."""
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary

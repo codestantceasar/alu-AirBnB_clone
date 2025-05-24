9d56ed1cbb6d3e1ab89644df9fa461bfc4d1fa20
codestantcaesar <c.akas@alustudent.com>
2025-05-25 01:54:59 +0200

    add this files

--- console.py
+++ console.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-import cmd
-from models.base_model import BaseModel
-from models import storage
-import shlex
+"""
+Entry point of the command interpreter.
+"""
 
-classes = {
-    "BaseModel": BaseModel
-}
+import cmd
 
 
 class HBNBCommand(cmd.Cmd):
+    """Command interpreter for the HBNB project."""
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
@@ -17,115 +15,13 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_EOF(self, arg):
-        """EOF to exit the program"""
+        """Exit the program on EOF (Ctrl+D)"""
         print()
         return True
 
     def emptyline(self):
-        """Do nothing when an empty line is entered"""
+        """Do nothing on empty input line"""
         pass
-
-    def do_create(self, arg):
-        """Create a new instance of BaseModel, saves it, and prints the id"""
-        if not arg:
-            print("** class name missing **")
-            return
-        if arg not in classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = classes[arg]()
-        new_instance.save()
-        print(new_instance.id)
-
-    def do_show(self, arg):
-        """Show the string representation of an instance"""
-        args = shlex.split(arg)
-        if not args:
-            print("** class name missing **")
-            return
-        if args[0] not in classes:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        key = f"{args[0]}.{args[1]}"
-        if key not in storage.all():
-            print("** no instance found **")
-            return
-        print(storage.all()[key])
-
-    def do_destroy(self, arg):
-        """Delete an instance based on the class name and id"""
-        args = shlex.split(arg)
-        if not args:
-            print("** class name missing **")
-            return
-        if args[0] not in classes:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        key = f"{args[0]}.{args[1]}"
-        if key not in storage.all():
-            print("** no instance found **")
-            return
-        del storage.all()[key]
-        storage.save()
-
-    def do_all(self, arg):
-        """Print all instances or all instances of a class"""
-        args = shlex.split(arg)
-        objects = storage.all()
-        if args:
-            if args[0] not in classes:
-                print("** class doesn't exist **")
-                return
-            print([str(obj) for key, obj in objects.items() if key.startswith(args[0] + ".")])
-        else:
-            print([str(obj) for obj in objects.values()])
-
-    def do_update(self, arg):
-        """Update an instance based on class name and id"""
-        args = shlex.split(arg)
-        if not args:
-            print("** class name missing **")
-            return
-        if args[0] not in classes:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        key = f"{args[0]}.{args[1]}"
-        if key not in storage.all():
-            print("** no instance found **")
-            return
-        if len(args) < 3:
-            print("** attribute name missing **")
-            return
-        if len(args) < 4:
-            print("** value missing **")
-            return
-
-        instance = storage.all()[key]
-        attr_name = args[2]
-        attr_value = args[3]
-
-        # Try to cast value to int or float
-        try:
-            if '.' in attr_value:
-                attr_value = float(attr_value)
-            else:
-                attr_value = int(attr_value)
-        except ValueError:
-            attr_value = attr_value.strip('"')
-
-        # Skip id, created_at, updated_at
-        if attr_name not in ("id", "created_at", "updated_at"):
-            setattr(instance, attr_name, attr_value)
-            instance.save()
 
 
 if __name__ == '__main__':
